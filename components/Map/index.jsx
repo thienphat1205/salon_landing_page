e2859f66f60f67f9da162988c62ac2bc062e6d21
phat.nguyen <phatnt@ghn.vn>
2022-03-08 08:19:49 +0700

    add backtop

--- components/Map/index.jsx
+++ components/Map/index.jsx
@@ -7,7 +7,6 @@ const Map = () => {
       <div data-aos="flip-up" className={s.container}>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3092.227122258779!2d-84.23741778464091!3d39.19228767952631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8840ffcf456c08b9%3A0xe2692a4e63f17c44!2sCT&#39;S%20Nail%20Spa!5e0!3m2!1svi!2s!4v1646620621564!5m2!1svi!2s"
-          allowfullscreen=""
           loading="lazy"
         ></iframe>
       </div>
--- layout/MainLayout/index.jsx
+++ layout/MainLayout/index.jsx
@@ -1,11 +1,33 @@
 import HeadHTML from "../../components/Head";
 import Header from "../../components/Header";
+import { BackTop } from "antd";
+import s from "./index.module.less";
+import { FaArrowUp } from "react-icons/fa";
+
+const style = {
+  height: 40,
+  width: 40,
+  lineHeight: "40px",
+  borderRadius: 4,
+  backgroundColor: "#1088e9",
+  color: "#fff",
+  textAlign: "center",
+  fontSize: 14,
+};
 
 const Layout = ({ children }) => {
   return (
     <>
       <HeadHTML />
       <Header />
+      <BackTop visibilityHeight={1500}>
+        <div className={s.backtop}>
+          <div className={s.content}>
+            <FaArrowUp style={{ fontSize: "20px", marginBottom: "5px" }} />
+            <p>TOP</p>
+          </div>
+        </div>
+      </BackTop>
       {children}
     </>
   );
